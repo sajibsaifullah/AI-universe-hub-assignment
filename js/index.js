f9d0b0d37f70890ecadf948e52f43c7778241064
sajibsaifullah <dusajibphy@gmail.com>
2023-03-02 22:07:58 +0600

    append card

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log('connected')
+const loadCardData = () => {
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+        .then(res => res.json())
+        .then(data => displayCardData(data.data.tools));
+}
+
+const displayCardData = (cards) => {
+    cards.forEach(card => {
+        const cardContainer = document.getElementById('card-container');
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML = `
+        <div class="card h-100">
+                <img src="${card.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        `;
+        cardContainer.appendChild(cardDiv);
+        console.log(card.image);
+    });
+}
+loadCardData();

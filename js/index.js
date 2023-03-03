778116f5a1b0b73ae382bc35a01f7e4f6d4013e5
sajibsaifullah <dusajibphy@gmail.com>
2023-03-03 19:05:21 +0600

    fixing

--- js/index.js
+++ js/index.js
@@ -34,7 +34,6 @@ const displayCardData = (cards) => {
         </div>
         `;
         cardContainer.appendChild(cardDiv);
-        // console.log(card);
     });
 }
 
@@ -76,8 +75,6 @@ const showModalData = data => {
     integrationContainer.appendChild(integrationUl);
 
     const modalImage = document.getElementById('modal-image');
-    // console.log(modalImage);
-    // const img = document.createElement('img');
     modalImage.innerHTML = `
         <img src="${data.data.image_link[0]}" class="card-img-top w-75 rounded-5" alt="...">
     `;
@@ -85,7 +82,6 @@ const showModalData = data => {
     document.getElementById('title').innerText = data.data.input_output_examples ? data.data.input_output_examples[0].input : "Can you give any example?";
     document.getElementById('text').innerText = data.data.input_output_examples ? data.data.input_output_examples[0].output : "No! Not Yet! Take a break!!!";
     
-    // console.log(data.data);
 }
 
 
